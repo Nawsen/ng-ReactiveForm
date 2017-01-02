e5363e0271440e6d38269073af2fbdaadfd79739
Nawsen <wannesvandorpe@hotmail.com>
2017-01-02 12:50:48 +0100

    basic form with submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  private form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    //nothing to be done here
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      subject: ['', Validators.required],
+      duration: [0, [Validators.required, Validators.pattern('[0-9]+')]]
+    });
+
+  }
+
+  submit() {
+    console.log(this.form.value);
+    this.form.reset();
+  }
 }
